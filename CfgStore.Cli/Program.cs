4caa96b76993336b6c4f62636ad27987b29c8a0c
Waldemar Tomme <waldetomme@gmail.com>
2023-09-21 14:24:11 +0200

    Add EnvironmentStepProvider

--- CfgStore.Cli/Program.cs
+++ CfgStore.Cli/Program.cs
@@ -6,9 +6,11 @@ using System.CommandLine.Parsing;
 using CfgStore.Application;
 using CfgStore.Application.Abstractions;
 using CfgStore.Cli.Implementations;
+using CfgStore.Modules.Environment;
 using CfgStore.Modules.Files;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using RT = LanguageExt.Sys.Live.Runtime;
 
 await BuildCommandLine()
@@ -21,6 +23,7 @@ await BuildCommandLine()
             services.AddSingleton<IManifestReader<RT>, ManifestReader<RT>>();
 
             services.AddSingleton<IPipelineStepProvider<RT>, FilesStepProvider<RT>>();
+            services.AddSingleton<IPipelineStepProvider<RT>, EnvironmentStepProvider<RT>>();
         }))
     .Build()
     .InvokeAsync(args);
@@ -45,26 +48,47 @@ CommandLineBuilder BuildCommandLine()
 (
     ICfgFileStore<RT> Store,
     IManifestReader<RT> ManifestReader,
-    Seq<IPipelineStepProvider<RT>> StepProviders
+    Seq<IPipelineStepProvider<RT>> StepProviders,
+    ILogger<Program> Logger
     ) Resolve(IHost host) =>
 (
     host.Services.GetRequiredService<ICfgFileStore<RT>>(),
     host.Services.GetRequiredService<IManifestReader<RT>>(),
-    host.Services.GetRequiredService<IEnumerable<IPipelineStepProvider<RT>>>().ToSeq()
+    host.Services.GetRequiredService<IEnumerable<IPipelineStepProvider<RT>>>().ToSeq(),
+    host.Services.GetRequiredService<ILogger<Program>>()
 );
 
-async Task InvokeStoreWorkflow(IHost host, CancellationToken cancellationToken)
+async Task InvokeWorkflow(Func<ICfgFileStore<RT>, Seq<IPipelineStepProvider<RT>>, IManifestReader<RT>, Aff<RT, Unit>> invocation, IHost host, CancellationToken cancellationToken)
 {
-    var (store, manifestReader, stepProviders) = Resolve(host);
+    var (store, manifestReader, stepProviders, logger) = Resolve(host);
     using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
     var runtime = RT.New(cts);
-    await StoreWorkflow<RT>.Execute(store, stepProviders, manifestReader).RunUnit(runtime);
+    var result = await invocation(store, stepProviders, manifestReader).Run(runtime);
+
+    result.IfFail(LogError);
+
+    void LogError(Error error)
+    {
+        switch (error)
+        {
+            case Exceptional exceptional:
+                logger.LogError(exceptional.ToException(), exceptional.Message);
+                break;
+
+            case ManyErrors manyErrors:
+                manyErrors.Errors.Iter(LogError);
+                break;
+
+            default:
+                logger.LogError(error.Exception.IfNoneUnsafe(default(Exception?)), error.Message);
+                error.Inner.Do(LogError);
+                break;
+        }
+    }
 }
 
-async Task InvokeLoadWorkflow(IHost host, CancellationToken cancellationToken)
-{
-    var (store, manifestReader, stepProviders) = Resolve(host);
-    using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-    var runtime = RT.New(cts);
-    await LoadWorkflow<RT>.Execute(store, stepProviders, manifestReader).RunUnit(runtime);
-}
+Task InvokeStoreWorkflow(IHost host, CancellationToken cancellationToken) =>
+    InvokeWorkflow(StoreWorkflow<RT>.Execute, host, cancellationToken);
+
+Task InvokeLoadWorkflow(IHost host, CancellationToken cancellationToken) =>
+    InvokeWorkflow(LoadWorkflow<RT>.Execute, host, cancellationToken);
---
+++ CfgStore.Modules.Environment/EnvironmentStepProvider.cs
@@ -0,0 +1,38 @@
+﻿using CfgStore.Application.Abstractions;
+using Map = LanguageExt.Map;
+
+namespace CfgStore.Modules.Environment;
+
+public class EnvironmentStepProvider<RT> : IPipelineStepProvider<RT>
+    where RT : struct, HasCancel<RT>
+{
+    public PipelineStep<RT> Load { get; } = (store, config, configs, next) =>
+        from _0 in unitEff
+        from cfg in ParseConfig(config)
+        from values in GetValues(cfg.Variables)
+        from _99 in next(store, configs)
+        select unit;
+
+    public string Name => "env";
+
+    public PipelineStep<RT> Store => Load;
+
+    private static Eff<Map<string, string>> GetValues(Seq<string> variables) =>
+        variables
+            .Select(x => Optional(System.Environment.GetEnvironmentVariable(x))
+                .ToEff($"Environment variable ${{{x}}} is not set.")
+                .Map(y => (x, y)))
+            .Traverse(identity)
+            .Map(x => Map.createRange(x));
+
+    private static Eff<Config> ParseConfig(PipelineStepConfig config) =>
+        from variables in config.Value.Get("variables").GetSeq()
+            .Select(x => x.Get())
+            .Traverse(identity)
+            .ToEff("Failed to parse variables from config.")
+        select new Config(
+            variables);
+
+    private record Config(
+        Seq<string> Variables);
+}
