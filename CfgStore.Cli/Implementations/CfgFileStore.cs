1280da97dfbb86d37ca2ff2f53ceeaa0a0f0fb22
Waldemar Tomme <waldetomme@gmail.com>
2023-09-21 14:50:00 +0200

    Log store access

--- CfgStore.Cli/Implementations/CfgFileStore.cs
+++ CfgStore.Cli/Implementations/CfgFileStore.cs
@@ -1,20 +1,36 @@
 ﻿using CfgStore.Application.Abstractions;
+using Microsoft.Extensions.Logging;
 
 namespace CfgStore.Cli.Implementations;
 
 internal class CfgFileStore<RT> : ICfgFileStore<RT>
     where RT : struct, HasCancel<RT>
 {
+    private readonly ILogger<CfgFileStore<RT>> logger;
+
+    public CfgFileStore(
+        ILogger<CfgFileStore<RT>> logger)
+    {
+        this.logger = logger;
+    }
+
     public Aff<RT, Seq<string>> List(string? path = default) =>
         Eff(() => Directory.EnumerateFiles(path ?? string.Empty, "*", SearchOption.AllDirectories)
             .ToSeq());
 
     public Aff<RT, string> ReadText(string path) => Aff((RT rt) =>
-        File.ReadAllTextAsync(path, rt.CancellationToken).ToValue());
+    {
+        logger.LogInformation("Reading {path} from store...", path);
+        return File.ReadAllTextAsync(path, rt.CancellationToken).ToValue();
+    });
 
     public Aff<RT, Unit> WriteText(string path, string content) =>
         from _0 in EnsureDirectoryExists(Path.GetDirectoryName(path))
-        from _1 in Aff((RT rt) => File.WriteAllTextAsync(path, content, rt.CancellationToken).ToUnit().ToValue())
+        from _1 in Aff((RT rt) =>
+        {
+            logger.LogInformation("Writing {path} to store...", path);
+            return File.WriteAllTextAsync(path, content, rt.CancellationToken).ToUnit().ToValue();
+        })
         select unit;
 
     private Eff<Unit> EnsureDirectoryExists(string? path) =>
