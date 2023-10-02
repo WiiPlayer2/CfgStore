861d15e2664534796c3899521b4724ab13cf3275
Waldemar Tomme <waldemar.tomme@bluehands.de>
2023-10-02 13:09:50 +0200

    Add shell step

---
+++ CfgStore.Modules.Shell/ShellStepProvider.cs
@@ -0,0 +1,13 @@
+﻿using CfgStore.Application.Abstractions;
+
+namespace CfgStore.Modules.Shell;
+
+public class ShellStepProvider<RT> : IPipelineStepProvider<RT>
+    where RT : struct, HasCancel<RT>
+{
+    public PipelineStep<RT> Load => throw new NotImplementedException();
+
+    public Seq<string> Names => Seq("shell", "cmd", "command", "commands");
+
+    public PipelineStep<RT> Store => throw new NotImplementedException();
+}
